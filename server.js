7e00baf560ff2268dc264688b61ce2514ec32969
Brad Williams <jb.wms89@gmail.com>
2020-07-27 16:51:40 -0400

    added some intial server files based off of the mongoose pizza app, will create heroku server next

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const apiRoute = require("./routes/apiRoute");
+const viewRoute = require("./routes/viewRoute");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+app.use(apiRoute);
+app.use(viewRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
