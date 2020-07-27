d8f7ecbc8e43d7d10ce3759db8af663593d73566
Brad Williams <jb.wms89@gmail.com>
2020-07-27 18:57:42 -0400

    removing public from view routes

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -2,18 +2,18 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/index.html"));
+    res.sendfile(path.join(__dirname, "../index.html"));
   });
 
   app.get("/exercise", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/exercise.html"));
+    res.sendfile(path.join(__dirname, "../exercise.html"));
   });
 
   app.get("/stats", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/stats.html"));
+    res.sendfile(path.join(__dirname, "../stats.html"));
   });
 
   app.get("/workout", function (req, res) {
-    res.sendfile(path.join(__dirname, "../public/workout.html"));
+    res.sendfile(path.join(__dirname, "../workout.html"));
   });
 };
